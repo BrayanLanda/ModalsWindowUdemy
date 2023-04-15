6869b9e3199cd3f2bd967414c4fbc94e4d91f482
Brayan Acosta <landakkpp@gmail.com>
2023-04-15 00:58:41 -0500

    se agrega evento de teclado para cerrar ventada de botoneria con Esc

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+
 const openModel = function(){
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
@@ -18,3 +19,10 @@ for(let i = 0; i < btnsOpenModal.length; i++)
     btnsOpenModal[i].addEventListener('click', openModel);
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+//realiza configuracion de evento de teclado 
+document.addEventListener('keydown', function(e){
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+        closeModal();
+    }
+})
